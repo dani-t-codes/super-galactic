f7d2aa8bbfb313a96f71e7584ddcc330826b334e
Danielle Thompson <danithompson74@gmail.com>
2020-11-13 11:16:18 -0800

    Add bootstrap, popper.js, and jQuery dependencies & update necessary files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
 import EarthlingData from './earthlingdatacruncher.js';
 
 $('#galactic-age-calculator').submit(function(event) {
