cc292c2828b4db50275ac91a41a3704019140f62
Danielle Thompson <danithompson74@gmail.com>
2020-11-13 10:55:29 -0800

    Add HTML webpack plugin & update related files

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import './css/styles.css';
 
 import EarthlingData from './earthlingdatacruncher.js';
 
-$(document).ready(function() {
-  $('#galactic-age-calculator').submit(function(event) {
-    event.preventDefault();
-    const earthAge = $('#earth-age').val();
-    const residence = $('#residence').val();
-    const gender = $('#gender').val();
-    const earthling = new EarthlingData(earthAge, residence, gender);
-    const response = earthling.crunchNumbers();
-    $('#response').append("<p>" + response + "</p>");
-  });
+$('#galactic-age-calculator').submit(function(event) {
+  event.preventDefault();
+  const earthAge = $('#earth-age').val();
+  const residence = $('#residence').val();
+  const gender = $('#gender').val();
+  const earthling = new EarthlingData(earthAge, residence, gender);
+  const response = earthling.crunchNumbers();
+  $('#response').append("<p>" + response + "</p>");
 });
