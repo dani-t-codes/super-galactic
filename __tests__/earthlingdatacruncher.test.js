630f92075855490e5cba5bd0b45f9f2624d898f7
Danielle Thompson <danithompson74@gmail.com>
2020-11-18 20:18:35 -0800

    Purposely fail earthling age test

--- __tests__/earthlingdatacruncher.test.js
+++ __tests__/earthlingdatacruncher.test.js
@@ -2,10 +2,8 @@ import EarthlingData from './../src/earthlingdatacruncher.js';
 
 describe('EarthlingData', () => {
 
-  test('should correctly return data with three demographics', () => {
-    const earthling = new EarthlingData('12','USA','F');
-    expect(earthling.earthAge).toEqual('12'); 
-    expect(earthling.residence).toEqual('USA');
-    expect(earthling.gender).toEqual('F'); 
+  test('should correctly return EarthlingData age', () => {
+    const earthling = new EarthlingData('12');
+    expect(earthling.earthAge).toEqual('13'); 
   });
 });
