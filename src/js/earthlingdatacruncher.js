64caa47f289b3869c26911fde44ae067fb3bc903
Danielle Thompson <danithompson74@gmail.com>
2020-11-21 19:41:52 -0800

    Purposely fail jupiterAge test again

--- src/js/earthlingdatacruncher.js
+++ src/js/earthlingdatacruncher.js
@@ -1,5 +1,21 @@
-export default class EarthlingData(earthAge, residence, gender) {
+export default function EarthlingData (earthAge, residence, gender) {
   this.earthAge = earthAge;
   this.residence = residence;
   this.gender = gender;
 }
+
+EarthlingData.prototype.mercuryAge = function() {
+  return this.earthAge * 0.24;
+};
+
+EarthlingData.prototype.venusAge = function() {
+  return this.earthAge * 0.62;
+};
+
+EarthlingData.prototype.marsAge = function() {
+  return this.earthAge * 1.88;
+};
+
+EarthlingData.prototype.jupiterAge = function() {
+  return this.earthAge * 11.86;
+};
