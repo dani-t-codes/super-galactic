88d41d6b325cf10c827d9ba00cf959233156dd71
Danielle Thompson <danithompson74@gmail.com>
2020-11-21 22:16:47 -0800

    Purposely fail life expectancy test

--- __tests__/earthlingdatacruncher.test.js
+++ __tests__/earthlingdatacruncher.test.js
@@ -10,31 +10,33 @@ describe('EarthlingData', () => {
   
   test('should correctly multiply earth age by 0.24 to get MercuryAge', () => {
     const earthling1 = new EarthlingData(26);
-    expect(earthling1.earthAge).toEqual(26);
     expect(earthling1.mercuryAge()).toEqual(6.24);
   });
   
   test('should correctly multiply earth age by 0.62 to get VenusAge', () => {
     const earthling2 = new EarthlingData(40);
-    expect(earthling2.earthAge).toEqual(40);
     expect(earthling2.venusAge()).toEqual(24.8);
   });
   
   test('should correctly multiply earth age by 1.88 to get MarsAge', () => {
     const earthling3 = new EarthlingData(40);
-    expect(earthling3.earthAge).toEqual(40);
     expect(earthling3.marsAge()).toEqual(75.19999999999999);
   });
 
   test('should correctly multiply earth age by 11.86 to get JupiterAge', () => {
     const earthling4 = new EarthlingData(40);
-    expect(earthling4.earthAge).toEqual(40);
     expect(earthling4.jupiterAge()).toEqual(474.4);
   });
   
+  test('should calculate U.S. life expectancy based on EarthAge', () => {
+    const earthling = new EarthlingData(40);
+    expect(earthling.lifeExpectancy()).toEqual('number');
+  });
+
 });
-    //bad test
-    // test('should correctly determine whether EarthlingData age is a number', () => {
-    //   const notEarthling = new EarthlingData("nerd");
-    //   expect(notEarthling.earthAge).toEqual(false);
-    // });
+
+//bad test
+// test('should correctly determine whether EarthlingData age is a number', () => {
+//   const notEarthling = new EarthlingData("nerd");
+//   expect(notEarthling.earthAge).toEqual(false);
+// });
