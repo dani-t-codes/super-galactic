7f8d94589b9f2fc171af17a21bea65faacc73b20
Danielle Thompson <danithompson74@gmail.com>
2020-11-21 19:22:01 -0800

    Purposely fail venusAge method test

--- __tests__/earthlingdatacruncher.test.js
+++ __tests__/earthlingdatacruncher.test.js
@@ -9,18 +9,18 @@ describe('EarthlingData', () => {
 
   
   test('should correctly multiply earth age by 0.24 to get MercuryAge', () => {
-    const earthling = new EarthlingData(26);
-    expect(earthling.earthAge).toEqual(26);
-    expect(earthling.mercuryAge()).toEqual(6.24);
+    const earthling1 = new EarthlingData(26);
+    expect(earthling1.earthAge).toEqual(26);
+    expect(earthling1.mercuryAge()).toEqual(6.24);
   });
-});
   
-  // EarthlingData.prototype.mercuryAge = function () {
-    //   this.earthAge *= 0.24;
-    //   console.log(this.mercuryAge);
-    // };
-
-
+  test('should correctly multiply earth age by 0.62 to get VenusAge', () => {
+    const earthling2 = new EarthlingData(40);
+    expect(earthling2.earthAge).toEqual(40);
+    expect(earthling2.venusAge()).toEqual(6.24);
+  });
+  
+});
     //bad test
     // test('should correctly determine whether EarthlingData age is a number', () => {
     //   const notEarthling = new EarthlingData("nerd");
