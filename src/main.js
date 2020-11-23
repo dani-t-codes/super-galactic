1854537b422a60d0974d2d3c1e59e6def415022b
Danielle Thompson <danithompson74@gmail.com>
2020-11-22 23:59:36 -0800

    Move showData actions back into main UI after bugs occured w/ separation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,20 +5,16 @@ import './css/styles.css';
 import EarthlingData from './js/EarthlingDataCruncher.js';
 
 //UI Logic
-
-function showData() {
-  $('#display-results').show();
-  $('input#earth-age').val("");
-  $('input#smoker-status').val("");
-}
-
 $(document).ready(function() {
-  showData();
   $('#galactic-age-calculator').submit(function(event) {
     event.preventDefault();
     const earthAge = $('#earth-age').val();
     const smokerStatus = $('#smoker-status').val();
     const earthling = new EarthlingData(`${earthAge}`, `${smokerStatus}`);
+    //clear forms, show hidden section
+    $('#display-results').show();
+    $('input#earth-age').val("");
+    $('input#smoker-status').val("");
     //age calculations
     $('#show-earth-age').html(`${earthAge}`);
     $('#show-mercury-age').html(earthling.mercuryAge());
