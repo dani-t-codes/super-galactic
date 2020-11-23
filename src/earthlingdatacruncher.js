dd0f004f591649fea40ca95319bf2c6cee668e04
Danielle Thompson <danithompson74@gmail.com>
2020-11-23 00:00:31 -0800

    Delete extra unneeded file

--- src/earthlingdatacruncher.js
+++
@@ -1,8 +0,0 @@
-export default class EarthlingData {
-  constructor(earthAge, residence, gender) {
-    this.earthAge = earthAge;
-    this.residence = residence;
-    this.gender = gender;
-  }
-
-} // end of 
